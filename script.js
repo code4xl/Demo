74ce02b41cc2f7cbe98a7c12140023fffe8ffa99
code4xl <haresh.kurade.2004@gmail.com>
2024-11-27 16:16:33 +0530

    oiooii

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("downloadBtn").addEventListener("click", () => {
     const link = document.createElement("a");
-    link.href = "public/errtygghdjghdkfj.zip"; // Path to the file
+    link.href = "/Demo/public/errtygghdjghdkfj.zip"; // Path to the file
     link.download = "yuiyiyuigghhjjj.zip";   // Name for the downloaded file
     link.click();
 });
