1e65bfb7989e04e9ec3a7a001b52886f8adddb84
code4xl <haresh.kurade.2004@gmail.com>
2024-11-27 16:46:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 document.getElementById("downloadBtn").addEventListener("click", () => {
     const link = document.createElement("a");
-    link.href = "/Demo/public/errtygghdjghdkfj.zip"; // Path to the file
-    link.download = "yuiyiyuigghhjjj.zip";   // Name for the downloaded file
+    link.href = "Demo/public/treasure.zip"; // Add /Demo before deploying....
+    link.download = "oioioioiiii.zip";
     link.click();
 });
+
+
+
+let enterPressCount = 0;
+        let timer;
+
+        document.addEventListener("keydown", (event) => {
+            if (event.key === "Enter") {
+                enterPressCount++;
+                if (timer) clearTimeout(timer);
+                timer = setTimeout(() => {
+                    enterPressCount = 0;
+                }, 2000);
+                if (enterPressCount === 5) {
+                    downloadFile();
+                    enterPressCount = 0;
+                }
+            }
+        });
+
+        function downloadFile() {
+            const link = document.createElement("a");
+            link.href = "public/treasure.zip";
+            link.download = "oioioioiiii.zip";
+            link.click();
+        }
